b0552818e6d6ab552f1124f8eae55b35bf85aecc
Mazennaja123 <63180234+Mazennaja123@users.noreply.github.com>
2022-09-16 20:55:44 +0300

    Adding javascript with testing event listeners

---
+++ scripts/script.js
@@ -0,0 +1,80 @@
+// prepare faces variables
+get_face1 = document.getElementsByClassName("face1");
+get_face2 = document.getElementsByClassName("face2");
+get_face3 = document.getElementsByClassName("face3");
+get_face4 = document.getElementsByClassName("face4");
+get_face5 = document.getElementsByClassName("face5");
+get_face6 = document.getElementsByClassName("face6");
+
+//prepare icons variables
+get_js_icon = document.getElementsByClassName("js_icon");
+get_css_icon = document.getElementsByClassName("css_icon");
+get_html_icon = document.getElementsByClassName("html_icon");
+
+const pics = [get_js_icon.src,get_css_icon.src,get_html_icon.src];
+//Starting game
+
+
+
+//display_cards();
+
+
+
+
+
+//Add listeners to Face cards
+get_face1[0].addEventListener("click",flip1);
+get_face2[0].addEventListener("click",flip2);
+get_face3[0].addEventListener("click",flip3);
+get_face4[0].addEventListener("click",flip4);
+get_face5[0].addEventListener("click",flip5);
+get_face6[0].addEventListener("click",flip6);
+
+
+function flip1(){
+
+
+console.log("test");
+
+}
+
+
+function flip2(){
+
+
+
+console.log("test");
+
+}
+
+
+function flip3(){
+
+
+
+console.log("test");
+
+}
+
+
+function flip4(){
+
+
+
+console.log("test");
+
+}
+
+
+function flip5(){
+
+
+console.log("test");
+
+}
+
+function flip6(){
+
+console.log("test");
+
+}
